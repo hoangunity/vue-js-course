f2cac84fc41fa819eef33afda840529342be6e64
hoangunity <hoangdm.141196@gmail.com>
2021-01-20 12:47:34 +0700

    Adding Basic Form Validation

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,13 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
       <input
         id="user-name"
         name="user-name"
         type="text"
         v-model.trim="userName"
+        @blur="validateInput"
       />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -117,7 +122,8 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: 'pending'
     };
   },
   methods: {
@@ -137,6 +143,13 @@ export default {
       this.how = null;
       console.log(`Confirm? ${this.confirm}`);
       this.confirm = false;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 };
@@ -154,6 +167,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
